1b95d21757fab618f04574ac708b3efd02f93d26
xjohjrdy <xjohjrdy@qq.com>
2024-04-17 17:46:40 +0800

    Create com.galaxy.stock.ts

---
+++ src/apps/com.galaxy.stock.ts
@@ -0,0 +1,11 @@
+{
+  id: 'com.galaxy.stock',
+  name: '银河证券',
+  groups: [
+    {
+      key: 0,
+      name: '关闭按钮',
+      rules: { matches: '[id="com.galaxy.stock:id/closeBtn"]' },
+    },
+  ],
+}

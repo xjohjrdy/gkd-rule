c1f0a025e91bcda3adfb627cfb36f2b0354f88f8
xjohjrdy <xjohjrdy@qq.com>
2025-10-20 14:45:50 +0800

    Create com.taobao.idlefish.ts

---
+++ src/apps/com.taobao.idlefish.ts
@@ -0,0 +1,14 @@
+import { defineGkdApp } from '@gkd-kit/define';
+export default defineGkdApp({
+  id: 'com.taobao.idlefish',
+  name: '闲鱼',
+  groups: [
+    {
+      key: 0,
+      name: '跳过广告',
+      rules: {
+        matches: ['[id="splash_ad_close"]'],
+      },
+    },
+  ],
+});
